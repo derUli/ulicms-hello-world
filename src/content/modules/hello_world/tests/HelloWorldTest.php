a59e51ad0a7da564f4246d9ead2a0ca54da2f5bf
Ulrich Schmidt <admin@deruli.de>
2019-01-03 19:07:54 +0100

    fix

--- src/content/modules/hello_world/tests/HelloWorldTest.php
+++ src/content/modules/hello_world/tests/HelloWorldTest.php
@@ -1,7 +1,7 @@
 <?php
 class HelloWorldTest extends \PHPUnit\Framework\TestCase {
 	public function testRender() {
-		$controller = ModuleHelper::getMainController ( "pi" );
+		$controller = ModuleHelper::getMainController ( "hello_world" );
 		$this->assertEquals ( "Hello World!", $controller->render () );
 	}
 }
